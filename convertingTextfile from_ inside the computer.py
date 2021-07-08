12126239ef1b8d072bb588a3438866e82dafbe06
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-07-08 12:27:21 +0400

    Update convertingTextfile from_ inside the computer.py

--- convertingTextfile from_ inside the computer.py
+++ convertingTextfile from_ inside the computer.py
@@ -1,9 +1,9 @@
 from gtts import gTTS
 import os
-fh = open("Test.txt","r")#file name of the text in your system
-myText =fh.read().replace("\n","")
-language ='en'
-output = gTTS(text=myText,lang = language,slow=False)
+fh = open("Test.txt","r")#using file handler function to open a text file name Test.txt in your system.
+myText =fh.read().replace("\n","")# function to read the file
+language ='en'# language selection 
+output = gTTS(text=myText,lang = language,slow=False) # format of the output file
 output.save("convertedoutput.mp3")# file name of your mp3 audiofile after conversion.
 fh.close()
 os.system("startoutput.mp3")
