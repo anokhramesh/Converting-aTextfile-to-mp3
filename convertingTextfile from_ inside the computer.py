c49e4720699eeb56d96f54b8368ef986710a7ba8
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-09-18 23:20:48 +0400

    Add files via upload

--- convertingTextfile from_ inside the computer.py
+++ convertingTextfile from_ inside the computer.py
@@ -1,9 +1,18 @@
+from tkinter import*
 from gtts import gTTS
 import os
-fh = open("Test.txt","r")#using file handler function to open a text file name Test.txt in your system.
-myText =fh.read().replace("\n","")# function to read the file
-language ='en'# language selection 
-output = gTTS(text=myText,lang = language,slow=False) # format of the output file
-output.save("convertedoutput.mp3")# file name of your mp3 audiofile after conversion.
-fh.close()
-os.system("startoutput.mp3")
+root=Tk()
+root.title("Text to sound converter")
+root.geometry("500x200")
+root.iconbitmap("python_icon.ico")
+def convert ():
+    fh = open("Test.txt","r")#name of the txt file on your system
+    myText =fh.read().replace("\n","")
+    language ='en'
+    output = gTTS(text=myText,lang = language,slow=True)
+    output.save("convertedoutput1.mp3")#name of your Audio mp3 file after conversion
+    fh.close()
+    #os.system("startoutput.mp3")
+convet_button=Button(root,text="Convert",font=("Helvetica",18),command=convert)
+convet_button.pack(pady=20)
+root.mainloop()
