9b706fe622faacf8965d748504f32535a3429a7c
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-09-18 23:22:38 +0400

    Update convertingTextfile from_ inside the computer.py

--- convertingTextfile from_ inside the computer.py
+++ convertingTextfile from_ inside the computer.py
@@ -13,6 +13,6 @@ def convert ():
     output.save("convertedoutput1.mp3")#name of your Audio mp3 file after conversion
     fh.close()
     #os.system("startoutput.mp3")
-convet_button=Button(root,text="Convert",font=("Helvetica",18),command=convert)
-convet_button.pack(pady=20)
+convert_button=Button(root,text="Convert Text to Mp3",font=("Helvetica",18),command=convert)
+convert_button.pack(pady=20)
 root.mainloop()
